f1b23f33decdb28c1626b72bbb44383bfb2aabe1
Tim Yu <tim.yu@madoor.com>
2021-07-23 01:08:12 -0700

    add some exercises

---
+++ src/main/java/edu/nidotim/exercise/amazon/ChooseFleets.java
@@ -0,0 +1,27 @@
+package edu.nidotim.exercise.amazon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChooseFleets {
+
+    public List<Integer> chooseFleets(List<Integer> wheels) {
+        List<Integer> result = new ArrayList<>();
+        for(Integer wheel : wheels) {
+            if(wheel % 2 != 0) {
+                result.add(0);
+                continue;
+            }
+
+            // 2 -> 2, 4 -> 2 2, 6 -> 2 2 2, 8 -> 2 2 2 2 , 10 -> 2 2 2 2 2
+            int numberOfTwos = wheel / 2;
+            if(numberOfTwos == 1) {
+                result.add(1);
+                continue;
+            }
+            int numberOfFours = numberOfTwos / 2;
+            result.add(numberOfFours + 1);
+        }
+        return result;
+    }
+}
